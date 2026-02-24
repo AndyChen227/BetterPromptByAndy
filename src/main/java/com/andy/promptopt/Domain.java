e227f784b7e942d342b1bc62f6c1a9101ffb12e5
Andy Chen <andy227@uw.edu>
2026-02-24 14:43:50 -0800

    M2.1 complete (domain + task + CLI)

---
+++ src/main/java/com/andy/promptopt/Domain.java
@@ -0,0 +1,9 @@
+package com.andy.promptopt;
+
+public enum Domain {
+    LINEAR_ALGEBRA,
+    PHYSICS,
+    CODING,
+    WRITING,
+    GENERAL
+}
---
+++ src/main/java/com/andy/promptopt/TaskType.java
@@ -0,0 +1,11 @@
+package com.andy.promptopt;
+
+public enum TaskType {
+    EXPLAIN,
+    SOLVE,
+    PROVE,
+    DEBUG,
+    SUMMARIZE,
+    PLAN,
+    GENERAL
+}
